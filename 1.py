6bc48bebc18508c51c27331a656f420c9db8c854
AlexJikaiYan <79490080+AlexJikaiYan@users.noreply.github.com>
2022-06-18 20:43:19 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,19 @@
+import csv 
+data=[] 
+with open("archive_dataset.csv") as f:
+    csvreader= csv.reader(f)
+    for row in csvreader:
+        data.append(row)
+
+headers=data[0]
+planetdata=data[1:]
+for datapoint in planetdata:
+    datapoint[2]=datapoint[2].lower()
+
+planetdata.sort(key=lambda planetdata:planetdata[2])
+with open("sorted.csv","a+") as f:
+    csvwriter=csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(planetdata)
+
+
---
+++ 2.py
@@ -0,0 +1,26 @@
+import csv
+dataset1=[]
+dataset2=[]
+
+with open("final.csv")as f:
+    csvreader=csv.reader(f)
+    for row in csvreader:
+        dataset1.append(row)
+
+with open("sorted.csv")as(f):
+    csvreader=csv.reader(f)
+    for row in csvreader:
+        dataset2.append(row)
+
+header1=dataset1[0]
+planetdata1=dataset1[1:]
+header2=dataset2[0]
+planetdata2=dataset2[1:]
+headers=header1+header2
+planetdata=[]
+for index, datarow in enumerate(planetdata1):
+    planetdata.append(planetdata1[index]+planetdata2[index])
+with open("sorted2.csv","a+") as f:
+    csvwriter=csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(planetdata)
